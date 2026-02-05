0ac7188ecc7f5bc8518d89fb93e62172a9154ac6
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2026-02-05 14:07:22 +1000

    Fix client-only class access on server side for fabric

--- src/fabric/java/com/joshiegemfinder/synchronisedblockstates/fabric/SynchronisedBlockstatesFabric.java
+++ src/fabric/java/com/joshiegemfinder/synchronisedblockstates/fabric/SynchronisedBlockstatesFabric.java
@@ -1,14 +1,9 @@
 package com.joshiegemfinder.synchronisedblockstates.fabric;
 
 import com.joshiegemfinder.synchronisedblockstates.common.SynchronisedBlockstates;
-import com.joshiegemfinder.synchronisedblockstates.common.client.handler.ChunkedRegistryHandler;
-import com.joshiegemfinder.synchronisedblockstates.common.client.handler.RegistryRemapHandler;
-import com.joshiegemfinder.synchronisedblockstates.fabric.mixin.client.ClientHandshakePacketListenerImplConnectionAccessor;
 import com.joshiegemfinder.synchronisedblockstates.fabric.network.SynchronisedBlockstatesNetworkFabric;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class SynchronisedBlockstatesFabric implements ModInitializer {
 	@Override
@@ -17,14 +12,5 @@ public class SynchronisedBlockstatesFabric implements ModInitializer {
 		
 		SynchronisedBlockstatesNetworkFabric.registerPackets();
 		SynchronisedBlockstatesNetworkFabric.registerTasks();
-		
-		ClientLoginConnectionEvents.INIT.register((handler, client) -> {
-			ChunkedRegistryHandler.clearDecoders();
-			RegistryRemapHandler.onLoginPhaseBegan(((ClientHandshakePacketListenerImplConnectionAccessor)handler).getConnection().isMemoryConnection());
-		});
-		
-		ClientPlayConnectionEvents.INIT.register((handler, client) -> {
-			RegistryRemapHandler.onLoginPhaseEnd(handler.getConnection().isMemoryConnection());
-		});
 	}
 }
--- src/fabric/java/com/joshiegemfinder/synchronisedblockstates/fabric/client/SynchronisedBlockstatesClientFabric.java
+++ src/fabric/java/com/joshiegemfinder/synchronisedblockstates/fabric/client/SynchronisedBlockstatesClientFabric.java
@@ -1,17 +1,30 @@
 package com.joshiegemfinder.synchronisedblockstates.fabric.client;
 
 import com.joshiegemfinder.synchronisedblockstates.common.client.SynchronisedBlockstatesClient;
+import com.joshiegemfinder.synchronisedblockstates.common.client.handler.ChunkedRegistryHandler;
+import com.joshiegemfinder.synchronisedblockstates.common.client.handler.RegistryRemapHandler;
+import com.joshiegemfinder.synchronisedblockstates.fabric.mixin.client.ClientHandshakePacketListenerImplConnectionAccessor;
 import com.joshiegemfinder.synchronisedblockstates.fabric.network.client.SynchronisedBlockstatesNetworkFabricClient;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientLoginConnectionEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 
 public class SynchronisedBlockstatesClientFabric implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
 		SynchronisedBlockstatesClient.onInitializeClient();
-//		dumpVanillaMappings();
 		
 		SynchronisedBlockstatesNetworkFabricClient.registerPackets();
+
+		ClientLoginConnectionEvents.INIT.register((handler, client) -> {
+			ChunkedRegistryHandler.clearDecoders();
+			RegistryRemapHandler.onLoginPhaseBegan(((ClientHandshakePacketListenerImplConnectionAccessor)handler).getConnection().isMemoryConnection());
+		});
+		
+		ClientPlayConnectionEvents.INIT.register((handler, client) -> {
+			RegistryRemapHandler.onLoginPhaseEnd(handler.getConnection().isMemoryConnection());
+		});
 	}
 }
